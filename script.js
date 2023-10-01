2b16a3fdc594b88f22e9bffbd10230605098c1a3
Anil Poul <118977274+Anilpoul@users.noreply.github.com>
2023-10-01 22:42:02 +0530

    Some changes Done

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container")
 }
+
+
+var userIcon = document.querySelector(".user-icon");
+var sidebar1 = document.querySelector(".sidebar1");
+var container = document.querySelector(".container");
+
+userIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container")
+}
